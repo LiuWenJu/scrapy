62a808cb758d6280e8e53540fdc37b4536a8895f
LiuWenJu <lwj037303@gmail.com>
2015-11-10 17:53:32 +0800

    add a project

--- sixvcrawl/sixvcrawl/spiders/6vhao.py
+++ sixvcrawl/sixvcrawl/spiders/6vhao.py
@@ -13,14 +13,21 @@ class A6vhaoSpider(scrapy.Spider):
         "http://www.6vhao.com/dy/2015-06-13/25484.html"
 
     ]
-
     def parse(self, response):
-        for sel in response.xpath('//ul/li'):
+        for sel in response.xpath('//tr//p'):
             item = SixvcrawlItem()
-            item['title'] = sel.xpath('a/text()').extract()
+            item['title'] = sel.xpath('/text()').extract()
             item['link'] = sel.xpath('a/@href').extract()
-            item['desc'] = sel.xpath('text()').extract()
+            item['desc'] = sel.xpath('//text()').extract()
             #print title, link, desc
+            #title = sel.xpath('/text()').extract()
+            #link = sel.xpath('a/@href').extract()
+            #desc = sel.xpath('//text()').extract()
+
+            #item['title'] = [t.encode('utf-8') for t in title]
+            #item['link'] = [l.encode('utf-8') for l in link]
+            #item['desc'] = [d.encode('utf-8') for d in desc]
+            #items.append(item)
             yield item
 
 
